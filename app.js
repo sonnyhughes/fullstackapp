8986f891c0244409f3b07e64f21433caf4503566
Sonny <richardhughesjr@gmail.com>
2018-02-24 10:38:55 -0600

    Initial Files with new Express boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var PORT = 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 app.listen(PORT, function() {
