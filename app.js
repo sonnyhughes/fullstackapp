058f0bfef9e5aed72c574f1bc8b501cd345463d6
Sonny <richardhughesjr@gmail.com>
2018-02-24 11:20:58 -0600

    Adds index.html, AJAX, and get methods to app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var path = require("path");
 var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    // res.send("Home Page!")
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api/all", function (req, res) {
+    // res.send("Home Page!")
+    res.send("some data");
+});
 
 
 app.listen(PORT, function() {
